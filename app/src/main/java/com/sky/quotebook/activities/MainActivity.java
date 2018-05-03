05c447412b129c478f4d3d7bd9412368bb12225d
yasirh33 <muhammmadyasir.2551@gmail.com>
2018-05-03 03:46:09 -0700

    Custom Dialog created, new changes

--- app/src/main/java/com/sky/quotebook/activities/MainActivity.java
+++ app/src/main/java/com/sky/quotebook/activities/MainActivity.java
@@ -1,14 +1,21 @@
 package com.sky.quotebook.activities;
 
 import android.app.ActivityOptions;
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Parcelable;
+import android.renderscript.RenderScript;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
@@ -16,9 +23,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -46,14 +56,17 @@ import com.sky.quotebook.fragments.AuthorListFragment;
 import com.sky.quotebook.fragments.CategoryListFragment;
 import com.sky.quotebook.fragments.SetupFragment;
 import com.sky.quotebook.interfaces.ItemClickListener;
+import com.sky.quotebook.model.AppPreferences;
 import com.sky.quotebook.model.Author;
+import com.sky.quotebook.model.Constant;
 import com.sky.quotebook.view.ItemTouchHelperCallback;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity implements GeneralDialogFragment.OnDialogFragmentClickListener{
+public class MainActivity extends AppCompatActivity implements GeneralDialogFragment.OnDialogFragmentClickListener {
 
     private RecyclerView mRecyclerView;
     private RecyclerViewAdapter adapter;
@@ -67,7 +80,8 @@ public class MainActivity extends AppCompatActivity implements GeneralDialogFrag
     BottomNavigationView bottomNavigation;
     Fragment fragment;
     FragmentManager fragmentManager;
-
+    TextView mlaunchCount;
+    Button mButtonNotify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,16 +90,22 @@ public class MainActivity extends AppCompatActivity implements GeneralDialogFrag
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
         setSupportActionBar(toolbar);
-
-
         getSupportActionBar().setTitle("main activity");
+
         //display back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getWindow().setGravity(toolbar.TEXT_ALIGNMENT_GRAVITY);
 
-        initData();
-        initView();
 
+        //launch rate app dialog
+        mlaunchCount = (TextView) findViewById(R.id.launchCount);
+        AppPreferences.getInstance(getApplicationContext()).incrementLaunchCount();
+        mlaunchCount.setText(getString(R.string.app_message,
+                AppPreferences.getInstance(getApplicationContext()).getLaunchCount()));
+        showRateAppDialog();
+
+
+        //BottomNavigationView for fragment author, category and note
         bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.inflateMenu(R.menu.bottom_menu);
         fragmentManager = getSupportFragmentManager();
@@ -105,32 +125,94 @@ public class MainActivity extends AppCompatActivity implements GeneralDialogFrag
                         break;
                 }
                 final FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+                //Custom animation on fragment appear and disappear
                 transaction.setCustomAnimations(R.anim.fragment_slide_left_enter,
                         R.anim.fragment_slide_left_exit,
                         R.anim.fragment_slide_right_enter,
                         R.anim.fragment_slide_right_exit);
 
                 transaction.replace(R.id.main_container, fragment);
+
+                //Back to host activity
                 transaction.addToBackStack(null);
                 transaction.commit();
                 return true;
             }
         });
 
+
+
+        initData();
+        initView();
     }
+
+    //rate us dialog
+    private void showRateAppDialog() {
+        boolean bool = AppPreferences.getInstance(getApplicationContext()).getAppRate();
+        int i = AppPreferences.getInstance(getApplicationContext()).getLaunchCount();
+        if ((bool) && (i == 2)) {
+            createAppRatingDialog(getString(R.string.rate_app_title), getString(R.string.rate_app_message)).show();
+        }
+    }
+
+    //created rate dialog, having three button, "Later", "Feedback" & "Rate Us"
+    private AlertDialog createAppRatingDialog(String rateAppTitle, String rateAppMessage) {
+        AlertDialog dialog = new AlertDialog.Builder(this).setIcon(R.drawable.ic_setup_red_white_60dp).
+                setPositiveButton(getString(R.string.dialog_app_rate), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface paramAnonymousDialogInterface, int paramAnonymousInt) {
+                        openAppInPlayStore(MainActivity.this);
+                        AppPreferences.getInstance(MainActivity.this.getApplicationContext()).setAppRate(false);
+                    }
+                }).setNegativeButton(getString(R.string.dialog_your_feedback), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface paramAnonymousDialogInterface, int paramAnonymousInt) {
+                openFeedback(MainActivity.this);
+                AppPreferences.getInstance(MainActivity.this.getApplicationContext()).setAppRate(false);
+            }
+        }).setNeutralButton(getString(R.string.dialog_ask_later), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface paramAnonymousDialogInterface, int paramAnonymousInt) {
+                paramAnonymousDialogInterface.dismiss();
+                AppPreferences.getInstance(MainActivity.this.getApplicationContext()).resetLaunchCount();
+            }
+        }).setMessage(rateAppMessage).setTitle(rateAppTitle).create();
+        return dialog;
+    }
+
+    //Path to our app on playStore
+    public static void openAppInPlayStore(Context paramContext) {
+        paramContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/")));
+    }
+
+    //user will email his/her feedBack on Emails Address only
+    public static void openFeedback(Context paramContext) {
+        Intent localIntent = new Intent(Intent.ACTION_SEND);
+        localIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"muhammadyasir.2551.@gmail.com"});
+        localIntent.putExtra(Intent.EXTRA_CC, "");
+        String str = null;
+        try {
+            str = paramContext.getPackageManager().getPackageInfo(paramContext.getPackageName(), 0).versionName;
+            localIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback for Your Android App");
+            localIntent.putExtra(Intent.EXTRA_TEXT, "\n\n----------------------------------\n Device OS: Android \n Device OS version: " +
+                    Build.VERSION.RELEASE + "\n App Version: " + str + "\n Device Brand: " + Build.BRAND +
+                    "\n Device Model: " + Build.MODEL + "\n Device Manufacturer: " + Build.MANUFACTURER);
+            localIntent.setType("message/rfc822");
+            paramContext.startActivity(Intent.createChooser(localIntent, "Choose an Email client :"));
+        } catch (Exception e) {
+            Log.d("OpenFeedback", e.getMessage());
+        }
+    }
+
 
     private void initData() {
         data = new ArrayList<>();
         for (int i = 1; i <= 20; i++) {
             data.add(i + "");
         }
-
         insertData = "0";
         loadTimes = 0;
     }
 
     private void initView() {
-
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view_1);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(linearLayoutManager);
@@ -140,7 +222,7 @@ public class MainActivity extends AppCompatActivity implements GeneralDialogFrag
         adapter.setItems(data);
         adapter.addFooter();
 
-        //support swapping left/Right, Move above/below
+        //support swapping left/Right --> delete a rec-view-item, Drag above/below --> to position an item,
         ItemTouchHelper.Callback callback = new ItemTouchHelperCallback(adapter);
         ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(callback);
         mItemTouchHelper.attachToRecyclerView(mRecyclerView);
@@ -184,14 +266,10 @@ public class MainActivity extends AppCompatActivity implements GeneralDialogFrag
         //add new items on scrolling
         mRecyclerView.addOnScrollListener(scrollListener);
     }
-    //end here
+    // rec-view ended here
 
-//commented because app crashes when back pressed two time
-  /*  @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-    }*/
 
+    //Menu Items, Toolbar items
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
@@ -204,32 +282,37 @@ public class MainActivity extends AppCompatActivity implements GeneralDialogFrag
             case R.id.action_settings: {
                 Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
 
+                //Starting a new activity will have these applied animations
                 startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
                 break;
             }
             case R.id.action_about: {
                 Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+
                 //animation applied
                 startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
                 break;
             }
 
             case R.id.action_save: {
+                //add your fav quote, author, and category
                 Toast.makeText(getApplicationContext(), "save", Toast.LENGTH_SHORT).show();
             }
             case R.id.action_share: {
-               // onShareClick(bottomNavigation);
+                // onShareClick(bottomNavigation);
                 GeneralDialogFragment generalDialogFragment =
-                        GeneralDialogFragment.newInstance("Really!! \n you've someone to share this? \n", "Here are two options");
-                generalDialogFragment.show(getSupportFragmentManager(),"dialog");
-              //  Toast.makeText(getApplicationContext(), "share", Toast.LENGTH_SHORT).show();
+                        GeneralDialogFragment.newInstance("Click 'share' to complete this action\n", "Here are two options");
+                generalDialogFragment.show(getSupportFragmentManager(), "dialog");
+                //  Toast.makeText(getApplicationContext(), "share", Toast.LENGTH_SHORT).show();
                 break;
             }
         }
         return false;
     }
 
-
+    /*
+    * "onShareClick" will only share your content on Facebook, Twitter and whatsApp
+    * */
     public void onShareClick(View v) {
         List<Intent> targetShareIntents = new ArrayList<Intent>();
         Intent shareIntent = new Intent();
@@ -240,7 +323,11 @@ public class MainActivity extends AppCompatActivity implements GeneralDialogFrag
             System.out.println("Have package");
             for (ResolveInfo resInfo : resInfos) {
                 String packageName = resInfo.activityInfo.packageName;
-                Log.i("Package Name", packageName);
+
+                //gets all of the installed packages names
+                Log.i("Package Name    ****** ", packageName);
+
+                //choose these three only to share
                 if (packageName.contains("com.twitter.android") || packageName.contains("com.facebook.katana") || packageName.contains("com.whatsapp")) {
                     Intent intent = new Intent();
                     intent.setComponent(new ComponentName(packageName, resInfo.activityInfo.name));
@@ -259,21 +346,22 @@ public class MainActivity extends AppCompatActivity implements GeneralDialogFrag
                 startActivity(chooserIntent);
             } else {
                 System.out.println("Do not Have Intent");
-                //showDialaog(this);
+                //show Dialog (this);
                 Toast.makeText(getApplicationContext(), "Don't share", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
+    //Dialog appear having the message and Button. Button "Share" --> share your content
     @Override
     public void onOkClicked(GeneralDialogFragment dialog) {
+        //called this method to share only on Twitter, Facebook and WhatsApp
         onShareClick(bottomNavigation);
-        Toast.makeText(getApplicationContext(), "this is on click Dialog fragment", Toast.LENGTH_SHORT).show();
     }
 
-    @Override
+    /*@Override
     public void onCancelClicked(GeneralDialogFragment dialog) {
         Toast.makeText(getApplicationContext(), "cancelled Dialog fragment", Toast.LENGTH_SHORT).show();
 
-    }
+    }*/
 }
